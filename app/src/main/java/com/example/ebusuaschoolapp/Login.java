f22539115a9c40447faa832f01dd510d786d3a13
michelleguti17 <gut20014@byui.edu>
2021-11-17 15:23:24 -0500

    Change of Name school

--- app/src/main/java/com/example/ebusuaschoolapp/Login.java
+++ app/src/main/java/com/example/ebusuaschoolapp/Login.java
@@ -1,30 +1,50 @@
 package com.example.ebusuaschoolapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
 
     Button createAccountBtn, loginBtn;
     EditText username, password; // to extract data
     FirebaseAuth firebaseAuth;
 
+    //Variable for the menu to show inside the login activity- Michelle
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Change Status Bar Color-Michelle
+        getWindow().setStatusBarColor(ContextCompat.getColor(Login.this,R.color.background_header_color));
 
         firebaseAuth = FirebaseAuth.getInstance(); // initialize
 //        Handle onClick event when create button is clicked
@@ -72,6 +92,35 @@ public class Login extends AppCompatActivity {
                 });
             }
         });
+
+
+        //Hooks for the menu - Michelle
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        textView =findViewById(R.id.textView);
+        toolbar = findViewById(R.id.toolbar);
+
+        //ToolBar
+
+
+        //Navigation Drawer Menu-Michelle
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle=new  ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    // Make Menu Return- Michelle
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 
     // check firebase authentication
@@ -85,4 +134,26 @@ public class Login extends AppCompatActivity {
             finish();
         }
     }
-}
+
+    //Navigation item Menu selected - Michelle
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch(menuItem.getItemId()){
+            case R.id.nav_home:
+                Intent home = new Intent(Login.this,MainActivity.class);
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+                break;
+
+            case R.id.nav_login:
+                Intent login = new Intent(Login.this,Login.class);
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+
+
+        }
+
+        return true;
+    }
+  }
--- app/src/main/java/com/example/ebusuaschoolapp/Register.java
+++ app/src/main/java/com/example/ebusuaschoolapp/Register.java
@@ -1,32 +1,49 @@
 package com.example.ebusuaschoolapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     EditText registerFullName, registerEmail, registerPassword, confirmPassword;
     Button registerUserBtn, gotoLogin;
 
     FirebaseAuth fAuth;
 
+    //Variable for the menu to show inside the login activity- Michelle
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Change Status Bar Color-Michelle
+        getWindow().setStatusBarColor(ContextCompat.getColor(Register.this,R.color.background_header_color));
 
         registerFullName = findViewById(R.id.registerFullName);
         registerEmail = findViewById(R.id.registerEmail);
@@ -98,5 +115,56 @@ public class Register extends AppCompatActivity {
             }
         });
 
+
+        //Hooks for the menu - Michelle
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        textView = findViewById(R.id.textView);
+        toolbar = findViewById(R.id.toolbar);
+
+        //ToolBar
+
+
+        //Navigation Drawer Menu-Michelle
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    // Make Menu Return- Michelle
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    //Navigation item Menu selected - Michelle
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                Intent home = new Intent(Register.this, MainActivity.class);
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+                break;
+
+            case R.id.nav_login:
+                Intent login = new Intent(Register.this, Login.class);
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+
+
+        }
+
+        return true;
     }
 }
